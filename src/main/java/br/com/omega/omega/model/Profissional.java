e3fbde3c9aa8693b8074f429228c379ba4b22c99
anderkallsgo <anderkallsgo@gmail.com>
2018-10-10 21:30:36 -0400

    Criação classe profissional

---
+++ src/main/java/br/com/omega/omega/model/Profissional.java
@@ -0,0 +1,24 @@
+package br.com.omega.omega.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Profissional implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome_profissinal", length = 255)
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
