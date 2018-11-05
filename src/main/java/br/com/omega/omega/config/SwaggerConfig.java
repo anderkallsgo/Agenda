ce728777ed407343a6822945322e51682dfb1fbb
adfigueiro <adfigueiro@fazenda.ms.gov.br>
2018-11-05 09:02:53 -0300

    Atualização swagger

--- src/main/java/br/com/omega/omega/config/SwaggerConfig.java
+++ src/main/java/br/com/omega/omega/config/SwaggerConfig.java
@@ -6,26 +6,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
 @EnableAutoConfiguration
 public class SwaggerConfig extends WebMvcConfigurationSupport{
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
+    public Docket productApi() {
+    return new Docket(DocumentationType.SWAGGER_2).select()
+            .apis(RequestHandlerSelectors.basePackage("br.com.omega.omega.resources")).paths(regex("/.*")).build()
+            .apiInfo(metaData());
+}
 
     private ApiInfo metaData() {
         return new ApiInfoBuilder().title("Spring Boot REST API")
@@ -42,4 +40,3 @@ public class SwaggerConfig extends WebMvcConfigurationSupport{
                 addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
-    
